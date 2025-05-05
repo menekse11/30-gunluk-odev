5053c5a24f3199aa79c0c2e9be35a033a99c3d19
menekse11 <mahmutmuzaffer11@hotmail.com>
2025-05-05 23:20:58 +0300

    Add files via upload

---
+++ PROBLEMLER/MergedExample.java
@@ -0,0 +1,57 @@
+/*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106 */
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    int[] merged = new int[nums1.length + nums2.length];
+        int i = 0, j = 0, k = 0;
+
+       
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        // Kalanları ekle
+        while (i < nums1.length) {
+            merged[k++] = nums1[i++];
+        }
+        while (j < nums2.length) {
+            merged[k++] = nums2[j++];
+        }
+
+        int total = merged.length;
+        if (total % 2 == 1) {
+            return merged[total / 2];
+        } else {
+            return (merged[total / 2 - 1] + merged[total / 2]) / 2.0;
+        }
+    }
+}
