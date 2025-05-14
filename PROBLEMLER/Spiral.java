90f8229301a502dac1180e6c560f67c0e00f27bb
menekse11 <mahmutmuzaffer11@hotmail.com>
2025-05-14 21:50:32 +0300

    Add files via upload

---
+++ PROBLEMLER/Spiral.java
@@ -0,0 +1,33 @@
+/*Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.*/
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1;
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+
+        while (num <= n * n) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            for (int i = right; i >= left; i--) {
+                matrix[bottom][i] = num++;
+            }
+            bottom--;
+
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][left] = num++;
+            }
+            left++;
+        }
+
+        return matrix;
+    }
+}
