0ef5ae23af46e175b9f223152bf2c7e2ccbcc63c
menekse11 <mahmutmuzaffer11@hotmail.com>
2025-05-22 16:38:54 +0300

    Add files via upload

---
+++ PROBLEMLER/wordBreak.java
@@ -0,0 +1,24 @@
+/*Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.*/
+
+ 
+
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
