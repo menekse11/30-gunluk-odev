10031ca7a33c4e19fcda2977daf28fd175a38a1a
menekse11 <mahmutmuzaffer11@hotmail.com>
2025-06-10 11:42:18 +0300

    Add files via upload

---
+++ PROBLEMLER/numberofIsland.java
@@ -0,0 +1,20 @@
+/*Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ */
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
